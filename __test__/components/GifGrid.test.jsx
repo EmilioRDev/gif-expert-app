d428b513de070570cd96ac5a89f0501dbe40d652
EmilioRDev <joseemilio0501@gmail.com>
2024-01-17 19:01:35 +0100

    Add test GifGrid

---
+++ __test__/components/GifGrid.test.jsx
@@ -0,0 +1,52 @@
+import { render, screen } from '@testing-library/react';
+import { GifGrid } from '../../src/components/GifGrid';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+
+
+jest.mock('../../src/hooks/useFetchGifs')
+
+describe('Pruebas en GifGrid', () => { 
+   
+   const category = 'One Piece'
+
+   test('debe de mostrar el loading inicialmente', () => {
+      
+
+      useFetchGifs.mockReturnValue({
+         images: [],
+         isLoading: true
+      })
+
+      render(<GifGrid category={category}/>);
+      expect(screen.getByText('Cargando...'));
+      expect(screen.getByText(category));
+
+   });
+
+   test('debe de mostrar items cuando se cargan las imágenes useFetchGifs', () => {
+      
+      const gifs = [
+         {
+            id: '12E',
+            title: 'Luffy',
+            url: 'https://hola/luffy'
+         },
+         {
+            id: 'T2E',
+            title: 'Zoro',
+            url: 'https://hola/zoro'
+         },
+      ]
+
+      useFetchGifs.mockReturnValue({
+         images: gifs,
+         isLoading: false
+      })
+
+      render(<GifGrid category={category}/>);
+      expect(screen.getAllByRole('img').length).toBe(2);
+      
+
+   });
+
+ })
--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,15 +1,17 @@
+import PropTypes from 'prop-types';
+
 import { GifItem } from './GifItem';
 import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({ category }) => {
 	
-	const {images, isLoding} = useFetchGifs(category);
+	const {images, isLoading} = useFetchGifs(category);
 
 	return (
 		<>
 			<h3>{category}</h3>
 			{
-				isLoding && (<h2>Cargando...</h2>)
+				isLoading && (<h2>Cargando...</h2>)
 			}
 			<div className="card-grid">
 				{images.map((image) => (
@@ -19,3 +21,7 @@ export const GifGrid = ({ category }) => {
 		</>
 	);
 };
+
+GifGrid.propTypes= {
+	category: PropTypes.string.isRequired,
+}
